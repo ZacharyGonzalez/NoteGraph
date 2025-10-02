100c8acd81e77f0ffdae8abebcc3419c0cee7dbf
Zack <venrite@gmail.com>
2025-10-02 19:18:15 -0400

    defining our function for the backend to see

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,9 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-
+import { myFirstFunction } from './backend/function/nlpKeywords/resource';
 defineBackend({
   auth,
   data,
+  myFirstFunction,
 });
