b1b5c9d4c617b071f9db987b38434299dd3060a5
Zack <venrite@gmail.com>
2025-10-02 14:49:16 -0400

    adding primative note creation and display from server

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,17 +1,10 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
-/*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any user authenticated via an API key can "create", "read",
-"update", and "delete" any "Todo" records.
-=========================================================================*/
 const schema = a.schema({
   Note: a
     .model({
-      title: a.string().required(),
-      content: a.string().required(),
-      keywords: a.string().array()
+      title: a.string(),
+      content: a.string(),
     })
     //.authorization((allow) => [allow.publicApiKey()]),
     .authorization(allow => [allow.owner()])
@@ -31,16 +24,6 @@ export const data = defineData({
   },
 });
 
-/*== STEP 2 ===============================================================
-Go to your frontend source code. From your client-side code, generate a
-Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
-WORK IN THE FRONTEND CODE FILE.)
-
-Using JavaScript or Next.js React Server Components, Middleware, Server 
-Actions or Pages Router? Review how to generate Data clients for those use
-cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
-=========================================================================*/
-
 /*
 "use client"
 import { generateClient } from "aws-amplify/data";
@@ -49,10 +32,6 @@ import type { Schema } from "@/amplify/data/resource";
 const client = generateClient<Schema>() // use this Data client for CRUDL requests
 */
 
-/*== STEP 3 ===============================================================
-Fetch records from the database and use them in your frontend component.
-(THIS SNIPPET WILL ONLY WORK IN THE FRONTEND CODE FILE.)
-=========================================================================*/
 
 /* For example, in a React component, you can use this snippet in your
   function's RETURN statement */
--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,53 @@ import TopBar from "./components/TopBar";
 import Draggable from 'react-draggable';
 import './layout.css'
 import createBlock from './components/Block';
+import { useEffect, useState } from 'react';
+import type { Schema } from "../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
+
+const client = generateClient<Schema>();
 
 function App() {
-  const {user, signOut} = useAuthenticator();
- 
+  const { user, signOut } = useAuthenticator();
+  const [notes, setNotes] = useState<Array<Schema["Note"]["type"]>>([]);
+
+  useEffect(() => {
+    async function fetchTodos() {
+      const result = await client.models.Note.list();
+      setNotes(result.data);
+    }
+
+    fetchTodos();
+  }, []);
+
+
+
+  async function createNote() {
+    const title = window.prompt("Note title");
+    const content = window.prompt("Note content");
+
+    if (title && content) {
+
+      await client.models.Note.create({
+        title,
+        content,
+      });
+    }
+  }
   return (
     <main>
-      <div className="layout-container">
-        <TopBar user = {user} signOut = {signOut} />
-        <Draggable>
-          <div>
-            {createBlock()}
-          </div>
-        </Draggable>
+      <button onClick={createNote}>New Note</button>
+
+      <div>
+        <TopBar user={user} signOut={signOut} />
+        <ul>
+          {notes.map((note) => (
+            <li key = {note.id}>
+              {note.title}
+              {note.content}
+            </li>
+          ))}
+        </ul>
       </div>
     </main>
   );
