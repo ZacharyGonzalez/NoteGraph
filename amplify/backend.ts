eef67ce36a287df285e7a1610a46d07cf85aceea
Zack <venrite@gmail.com>
2025-10-02 19:30:56 -0400

    undoing whatever damage i just did

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,9 +1,7 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-import { myFirstFunction } from './backend/function/nlpKeywords/resource';
 defineBackend({
   auth,
   data,
-  myFirstFunction,
 });
--- amplify/backend/function/nlpKeywords/resource.ts
+++
@@ -1,6 +0,0 @@
-import { defineFunction } from "@aws-amplify/backend";
-
-export const myFirstFunction = defineFunction({
-  name: "handler.py",
-  entry: "./handler.ts"
-});
