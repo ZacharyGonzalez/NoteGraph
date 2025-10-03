5d19e886fd8011a0d409de6ae4a4a953690c9c0f
Zack <venrite@gmail.com>
2025-10-03 14:25:30 -0400

    removing backend.ts syntax error

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,9 +1,7 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-import { myApiFunction  } from './backend/function/nlpKeywords/resource';
 defineBackend({
   auth,
   data,
-  myApiFunction,
 });
