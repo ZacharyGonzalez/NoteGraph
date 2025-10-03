4c8a17f5e8c7b7fa3743d3260e3d1118ba122876
Zack <venrite@gmail.com>
2025-10-03 16:00:02 -0400

    API removal as its been an issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useAuthenticator } from '@aws-amplify/ui-react';
 import { useEffect, useState } from 'react';
 import { generateClient } from "aws-amplify/data";
-import { RestApi } from 'aws-cdk-lib/aws-apigateway';
 
 import type { Schema } from "../amplify/data/resource";
 import Block from './components/Block';
@@ -32,23 +31,10 @@ function App() {
       });
     }
   }
-  RestApi
-  async function callLambda() {
-    const res = await fetch('https://660o9213q2.execute-api.us-east-2.amazonaws.com/dev/keywords', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ keywords: "data" })
-    });
 
-    const data = await res.json();
-    console.log(data);
-  }
   return (
     <main>
       <button onClick={createNote}>New Note</button>
-      <button onClick={() => callLambda()}>
-        Call Lambda
-      </button>
       <div>
         <TopBar user={user} signOut={signOut} />
         {notes.map((note) => (
