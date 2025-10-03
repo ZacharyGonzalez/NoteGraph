70967dcd729c229885eee84e58918ffa6495fcde
Zack <venrite@gmail.com>
2025-10-03 03:17:22 -0400

    grep found handler.ts remnants that are breaking my build

---
+++ amplify/backend/function/nlpKeywords/handler.py
@@ -0,0 +1,13 @@
+
+def handler(event, context):
+    print("event:", event)
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+        },
+        "body": '"Hello from myFunction!"'
+    }
